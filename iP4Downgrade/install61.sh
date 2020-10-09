79c11375f377072798cd29ee5f6add8c7952e6a0
80036nd <40246027+80036nd@users.noreply.github.com>
2020-10-09 19:30:49 -0400

    Add files via upload

---
+++ iP4Downgrade/install61.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Documents/CodeNameM8/iP4Downgrade/
+./restore.sh ~/Downloads/iPhone3,1_6.1_10B144_Restore_Patched.ipsw
+killall Terminal
---
+++ iP4Downgrade/make6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Documents/CodeNameM8/iP4Downgrade/
+./make_ipsw.sh ~/Downloads/iPhone3\,1_6.0_10A403_Restore.ipsw
+killall Terminal
