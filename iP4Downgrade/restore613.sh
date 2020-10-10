9f236e3e9a056f0cb4371873f037189f5e79461e
80036nd <40246027+80036nd@users.noreply.github.com>
2020-10-10 18:51:50 -0400

    Update restore613.sh

--- iP4Downgrade/restore613.sh
+++ iP4Downgrade/restore613.sh
@@ -2,5 +2,6 @@
 cd ~/Documents/CodeNameM8/iP4Downgrade/
 chmod 777 restore.sh
 chmod 777 ./tools/tsschecker
+chmod 777 ./ipwndfu
 ./restore.sh ~/Downloads/iPhone3,1_6.1.3_10B329_Restore_Patched.ipsw
 killall Terminal
