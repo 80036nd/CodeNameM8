caca1e3e7ecbce9abe43d43c21ff4bfc3affbadc
80036nd <40246027+80036nd@users.noreply.github.com>
2020-10-10 19:04:41 -0400

    Update make_ipsw.sh

--- iP4Downgrade/make_ipsw.sh
+++ iP4Downgrade/make_ipsw.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
+chmod 777 ./tools/xpwntool
+chmod 777 ./tools/iBoot32Patcher/iBoot32Patcher
+chmod 777 ./tools/hexpatch.sh
+chmod 777 ./tools/root_tar/mytar
+chmod 777 ./tools/ipsw
 
 realpath() {
     canonicalize_path "$(resolve_symlinks "$1")"
