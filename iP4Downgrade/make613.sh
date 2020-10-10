6776359da20b85e17efe93eab5ed4fc258dca690
80036nd <40246027+80036nd@users.noreply.github.com>
2020-10-10 14:14:30 -0400

    Update make613.sh

--- iP4Downgrade/make613.sh
+++ iP4Downgrade/make613.sh
@@ -5,5 +5,6 @@ chmod 777 ./tools/xpwntool
 chmod 777 ./tools/iBoot32Patcher/iBoot32Patcher
 chmod 777 ./tools/hexpatch.sh
 chmod 777 ./tools/root_tar/mytar
+chmod 777 ./tools/ipsw
 ./make_ipsw.sh ~/Downloads/iPhone3\,1_6.1.3_10B329_Restore.ipsw
 killall Terminal
